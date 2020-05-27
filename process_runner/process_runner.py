1f43213da9ad8f2486155d695572e1ceb880669e
Charlemagne Wong <frian13@hotmail.com>
2020-05-27 16:01:49 -0500

    WIP code

--- process_runner/process_runner.py
+++ process_runner/process_runner.py
@@ -5,6 +5,8 @@ from secrets import token_urlsafe
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import config_version_manager as cvm
+
 app = Flask(__name__)
 CORS(app)
 
@@ -17,20 +19,30 @@ try:
     file.close()
     print('Loaded config file')
 
+
+    
 except OSError as err:
     print("Unable to find config.json. Setting config as empty")
-    config = {
-        'host': '0.0.0.0',
-        'port': 8010,
-        'processname': 'program'
-    }
+    config = cvm.new_config()
 
     with open('config.json', 'w+') as file:
         json.dump(config, file)
+
 except Exception as err:
     print(err)
     exit()
 
+# todo start only program in config file
+@app.route('/api/<string:token>/start/', methods=['GET', 'POST'])
+def api_start(token):
+    if request.method == 'POST':
+        content = request.get_json()
+        
+        if 'program' in content and 'program':
+            subprocess.Popen()
+        # if 'program' not in request.
+
+    return 'Done'
 
 if __name__ in '__main__':
     host = config['host'] if 'host' in config else '0.0.0.0'
@@ -40,9 +52,11 @@ if __name__ in '__main__':
         token = config['token']
     else:
         token = token_urlsafe(32)
+        config['token'] = token
 
     # update config file
+    # todo only save file if config in memory is different from file
     with open('config.json', 'w') as file:
         json.dump(config, file)
     
-    app.run(host='0.0.0.0', port=8010)
+    app.run(host=host, port=port)
